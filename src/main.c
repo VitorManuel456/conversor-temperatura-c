2c757fdf72979aeab42332d6f23661e190673f8a
VitorManuel456 <165670280+VitorManuel456@users.noreply.github.com>
2026-07-08 00:53:20 -0300

    Create project structure

---
+++ src/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+
+    printf("=================================\n");
+    printf(" Conversor de Temperatura em C\n");
+    printf("=================================\n");
+
+    return 0;
+
+}
